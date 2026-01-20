ddf6035efde9f02a28c39f484c070f551fe89d43
Priya Mahyanshi <priyamahyanshi150319@gmail.com>
2026-01-20 12:56:32 +0000

    created Installation Request Master and Instrument Application Master doctypes in Services app

---
+++ servicesapp/servicesapp/doctype/installation_request_master/installation_request_master.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Meril and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Installation Request Master", {
+// 	refresh(frm) {
+
+// 	},
+// });
