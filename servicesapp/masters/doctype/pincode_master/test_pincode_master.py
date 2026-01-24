499982e8fca2117ba8095ed15b2a0cec31a8bd4e
Rishi-Hingad <rishi.hingad@merillife.com>
2026-01-24 12:27:23 +0530

    Add company Master, State Master, City Master and Pincode Master

---
+++ servicesapp/masters/doctype/pincode_master/test_pincode_master.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Blue Phoenix and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPincodeMaster(FrappeTestCase):
+	pass
