6ddd7a1f0898a95342f08db4ab4f242348a738f8
Rishi-Hingad <rishi.hingad@merillife.com>
2026-01-27 10:22:26 +0530

    Changed the name of DIstributor Master and modifed asset master and account master

--- servicesapp/masters/doctype/zone_master/test_zone_master.py
+++ servicesapp/masters/doctype/zone_master/test_zone_master.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestDistributerMaster(FrappeTestCase):
+class TestZoneMaster(FrappeTestCase):
 	pass
--- servicesapp/masters/doctype/zone_master/zone_master.py
+++ servicesapp/masters/doctype/zone_master/zone_master.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class DistributerMaster(Document):
+class ZoneMaster(Document):
 	pass
