7947721f103ca855a26c828b95bb0d753fd067ef
mintvinylsky <318msahu@gmail.com>
2026-01-28 06:06:28 +0000

    date logic script and a few other changes

--- servicesapp/public/js/instrument_application_master.js
+++ servicesapp/public/js/instrument_application_master.js
@@ -4,5 +4,11 @@ frappe.ui.form.on('Instrument Application Master', {
 	        frm.set_value('instrument_application_owner', frappe.session.user_fullname);
 	    }
 	    
-	}
+	},
+
+	before_save: function(frm) {
+        if (!frm.doc.call_closure_date) {
+            frm.set_value('call_closure_date', frappe.datetime.get_today());
+        }
+    }
 })
