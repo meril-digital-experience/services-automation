876f9ad1494e4b2c2c652bc1db473d985b9f6124
Rishi-Hingad <rishi.hingad@merillife.com>
2026-01-31 16:54:06 +0530

    Change in Account Master JS file and some in Product Master Doctype

--- servicesapp/public/js/account_master.js
+++ servicesapp/public/js/account_master.js
@@ -5,3 +5,19 @@ frappe.ui.form.on('Account Master', {
         }
     }
 })
+
+// frappe.ui.form.on('Account Master', {
+//     onload(frm) {
+//         if (!frm.is_new()) return;
+
+//         frappe.db.get_value(
+//             'Employee',
+//             { company_email: frappe.session.user },
+//             'name'
+//         ).then(r => {
+//             if (r && r.message && r.message.name) {
+//                 frm.set_value('account_owner', r.message.name);
+//             }
+//         });
+//     }
+// });
