fb1b2b1af42037cbed696e65288bb4ad4accb5a1
Rishi-Hingad <rishi.hingad@merillife.com>
2026-02-03 16:46:22 +0530

    Changes in naming series, rr applciaton master and other call

--- servicesapp/masters/doctype/employee/employee.js
+++ servicesapp/masters/doctype/employee/employee.js
@@ -1,8 +1,32 @@
 // Copyright (c) 2026, Meril and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Employee", {
-// 	refresh(frm) {
+frappe.ui.form.on("Employee", {
+    salutation(frm) {
+        set_employee_name(frm);
+    },
+    first_name(frm) {
+        set_employee_name(frm);
+    },
+    last_name(frm) {
+        set_employee_name(frm);
+    }
+});
 
-// 	},
-// });
+function set_employee_name(frm) {
+    let parts = [];
+
+    if (frm.doc.salutation) {
+        parts.push(frm.doc.salutation);
+    }
+
+    if (frm.doc.first_name) {
+        parts.push(frm.doc.first_name);
+    }
+
+    if (frm.doc.last_name) {
+        parts.push(frm.doc.last_name);
+    }
+
+    frm.set_value("employee_name", parts.join(" "));
+}
--- servicesapp/public/js/asset_master.js
+++ servicesapp/public/js/asset_master.js
@@ -1,33 +1,34 @@
 frappe.ui.form.on('Asset Master', {
-    onload: function(frm) {
-        if (frm.is_new) {
+    onload(frm) {
+        if (frm.is_new()) {
             frm.set_value('asset_owner', frappe.session.user_fullname);
         }
     },
 
-    product_name: function(frm) {
-        if (frm.doc.product_name) {
-            
-            frappe.db.get_value('Product Master', frm.doc.product_name, 'product_short_code', (r) => {
-                if (r && r.product_short_code) {
-                    
-                    let short_code = r.product_short_code.replace('-', ''); 
-                    
-                   
-                    let random_part = Math.floor(100000 + Math.random() * 900000).toString();
-                    
-                    
-                    let combined = short_code + random_part;
-                    let final_serial = combined.substring(0, 9);
-                    
-                    
-                    frm.set_value('serial_no', final_serial);
-                }
-            });
-        }
+    product_name(frm) {
+        if (!frm.doc.product_name) return;
+
+        frappe.db.get_value('Product Master', frm.doc.product_name, 'product_short_code', (r) => {
+                if (!r || !r.product_short_code) return;
+
+                let short_code = r.product_short_code.replace(/[^A-Za-z0-9]/g, '');
+
+                short_code = short_code.substring(0, 3).toUpperCase();
+
+                let remaining_length = 9 - short_code.length;
+
+                let random_part = Math.random()
+                    .toString()
+                    .slice(2, 2 + remaining_length);
+
+                let final_serial = short_code + random_part;
+
+                frm.set_value('serial_no', final_serial);
+            }
+        );
     },
 
-    before_save: function(frm) {
+    before_save(frm) {
         if (!frm.doc.asset_name || frm.doc.asset_name.includes("new-asset-master")) {
             let asset_random = Math.floor(10000 + Math.random() * 90000);
             frm.set_value('asset_name', "MDPL00" + asset_random);
