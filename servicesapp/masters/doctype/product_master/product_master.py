eb765d3bfb5f408fa3d48c8d18936ac0221cdd43
Rishi-Hingad <rishi.hingad@merillife.com>
2026-03-01 19:27:44 +0530

    Product Master Changes and User Workspace

--- servicesapp/masters/doctype/product_master/product_master.py
+++ servicesapp/masters/doctype/product_master/product_master.py
@@ -3,18 +3,12 @@
 
 import frappe
 from frappe.model.document import Document
-
-class ProductMaster(Document):
-    pass
-
-
-import frappe
+from frappe.utils import getdate
 from collections import defaultdict
 from datetime import datetime
 
-import frappe
-
-import frappe
+class ProductMaster(Document):
+    pass
 
 @frappe.whitelist()
 def get_user_monthly_assigned_calls():
@@ -65,3 +59,55 @@ def get_user_monthly_assigned_calls():
             result[month_key][dt] += 1
 
     return result
+
+
+@frappe.whitelist()
+def get_monthly_call_status_trend():
+
+    doctypes = [
+        "Instrument Application Master",
+        "Installation Request Master",
+        "Instrument Breakdown Master",
+        "RR Application Call Master",
+        "Other Calls Issue Master"
+    ]
+
+    result = defaultdict(lambda: {
+        "Assigned": 0,
+        "In Progress": 0,
+        "Finished": 0
+    })
+
+    for dt in doctypes:
+
+        records = frappe.get_all(
+            dt,
+            fields=["call_schedule_date", "call_status"],
+            filters={
+                "docstatus": ["!=", 2]
+            }
+        )
+
+        for r in records:
+            if not r.call_schedule_date:
+                continue
+
+            month_key = getdate(r.call_schedule_date).replace(day=1)
+            month_str = month_key.strftime("%Y-%m-01")
+
+            status = (r.call_status or "").strip()
+
+            if status in ["Assigned", "In Progress", "Finished"]:
+                result[month_str][status] += 1
+
+    final_result = {}
+
+    for month, statuses in result.items():
+        final_result[month] = {
+            "Assigned": statuses.get("Assigned", 0),
+            "In Progress": statuses.get("In Progress", 0),
+            "Finished": statuses.get("Finished", 0),
+        }
+
+    frappe.response["message"] = dict(sorted(final_result.items()))
+
