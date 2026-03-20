953c3e93a2868aefece191c99e7c02a5ca491dfa
Rishi-Hingad <rishi.hingad@merillife.com>
2026-03-20 17:57:37 +0530

    Add call assigned email in the service

--- servicesapp/masters/doctype/account_master/account_master.js
+++ servicesapp/masters/doctype/account_master/account_master.js
@@ -1,8 +1,26 @@
 // Copyright (c) 2026, Meril and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Account Master", {
-// 	refresh(frm) {
+frappe.ui.form.on("Account Master", {
+    onload: function(frm) {
+        if (frappe.session.user !== "Administrator") {
+            frm.set_df_property("company", "hidden", 1);
+        }
+    },
 
-// 	},
-// });
+    meril_division: function(frm) {
+        if (!frm.doc.meril_division) return;
+
+        frappe.db.get_value("Company", 
+            { company_name: frm.doc.meril_division }, 
+            "name"
+        ).then(r => {
+            if (r.message && r.message.name) {
+                frm.set_value("company", r.message.name);
+            } else {
+                frappe.msgprint("No Company found for this Meril Division");
+                frm.set_value("company", null);
+            }
+        });
+    }
+});
--- servicesapp/servicesapp/doctype/rr_application_call_master/rr_application_call_master.py
+++ servicesapp/servicesapp/doctype/rr_application_call_master/rr_application_call_master.py
@@ -3,11 +3,28 @@
 
 import frappe
 from frappe.model.document import Document
-from servicesapp.utils import assign_engineer
+from servicesapp.utils.assign_engineer import assign_engineer
+
 
 class RRApplicationCallMaster(Document):
     pass
-    #def after_insert(self):
-     #   assign_engineer(self)
-      #  self.db_set("assigned_engineer", self.assigned_engineer)
+    def after_insert(self):
+       assign_engineer(self)
+       self.db_set("assigned_engineer", self.assigned_engineer)
+
+    def autoname(self):
+        last = frappe.get_all(
+            "RR Application Call Master",
+            fields=["name"],
+            order_by="name desc",
+            limit=1
+        )
+
+        if last:
+            last_num = int(last[0]["name"].replace("ARR", ""))
+            new_num = last_num + 1
+        else:
+            new_num = 1
+
+        self.name = f"ARR{str(new_num).zfill(10)}"
     
